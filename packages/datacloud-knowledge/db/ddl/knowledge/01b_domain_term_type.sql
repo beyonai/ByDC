6110cd8b749414a6b1e47d4b9c2106d5a840a124
Pliosauroideas <Pliosauroide@protonmail.com>
2026-07-12 21:29:50 +0800

    refactor(knowledge): 删除旧 domain 表 DDL，迁移至 term_domain 设计

--- packages/datacloud-knowledge/db/ddl/knowledge/01b_domain_term_type.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS domain_term_type (
-    domain_id VARCHAR(64) NOT NULL,
-    type_code VARCHAR(32) NOT NULL,
-    PRIMARY KEY (domain_id, type_code)
-);
-
-COMMENT ON TABLE domain_term_type IS '领域-术语类型关联表：支持领域与术语类型的多对多关系';
-COMMENT ON COLUMN domain_term_type.domain_id  IS '领域ID';
-COMMENT ON COLUMN domain_term_type.type_code  IS '术语类型编码';
